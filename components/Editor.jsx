95c7c32a55e1d129aebc43cb01de2d1449d30bb6
Abhip32 <abhishek.a.patil112@gmail.com>
2023-07-02 18:00:10 +0530

    Editer Improved

--- components/Editor.jsx
+++ components/Editor.jsx
@@ -1,54 +1,40 @@
-import React, { useState } from 'react'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/material-darker.css'
-import 'codemirror/theme/monokai.css'
-import 'codemirror/mode/xml/xml'
-import 'codemirror/mode/javascript/javascript'
-import 'codemirror/mode/css/css'
-import { Controlled as ControlledEditor } from 'react-codemirror2'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
-import "@/styles/Editor.css" 
+import React, { useState } from 'react';
+import { Editor as ControlledEditor } from '@monaco-editor/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import '@/styles/Editor.css';
 
 export default function Editor(props) {
-    const {
-        language,
-        displayName,
-        value,
-        onChange
-    } = props
-    const [open, setOpen] = useState(true)
+  const { language, displayName, value, onChange } = props;
+  const [open, setOpen] = useState(true);
 
-    function handleChange(editor, data, value) {
-        onChange(value)
-    }
+  function handleChange(value) {
+    onChange(value);
+  }
+
 
   return (
     <div className={`editor-container${open ? '' : 'collapsed'}`}>
-        <div className='editor-name'>
-         
-            {displayName}
-            <button
-            type='button'
-            className='expand-collapse-btn'
-                onClick={() => setOpen(prevOpen => !prevOpen)}
-            >
-                <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
-            </button>
-        </div>
-        <ControlledEditor 
-            onBeforeChange={handleChange}
-            value={value}
-            className="editor-wrapper"
-            options={{
-                lineWrapping: true,
-                lint: true,
-                mode: language,
-                theme: 'material-darker',
-                
-                lineNumbers: true
-            }}
-        />
+      <div className='editor-name'>
+        {displayName}
+        <button
+          type='button'
+          className='expand-collapse-btn'
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
+      </div>
+      <ControlledEditor
+        height="40vh"
+        value={value}
+        onChange={handleChange}
+        language={language}
+        className='editor-wrapper'
+        theme='vs-dark'
+      
+      />
     </div>
-  )
+  );
 }
